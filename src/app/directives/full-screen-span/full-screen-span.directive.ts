6b3c814ed42ecc7e2bc86c41dda048bde9829acd
ks-mani <kumar.mani017@gmail.com>
2021-03-05 14:53:29 +0530

    Section #1 | Step #1.2 | Addition of code to Full Screen Span Directive

--- src/app/directives/full-screen-span/full-screen-span.directive.ts
+++ src/app/directives/full-screen-span/full-screen-span.directive.ts
@@ -1,6 +1,24 @@
-import { Directive } from "@angular/core";
+import { Directive, ElementRef, HostListener } from "@angular/core";
 
 @Directive({
   selector: "[fullScreenSpan]"
 })
-export class FullScreenSpan {}
+export class FullScreenSpan {
+  private timerId: any; // Debouncinig Variable
+  constructor(private elRef: ElementRef) {}
+
+  ngOnInit(): void {
+    this.elRef.nativeElement.style.height = window.innerHeight - 16 + "px";
+    this.elRef.nativeElement.style.width = "100%";
+  }
+
+  @HostListener("window:resize")
+  onWindowResize() {
+    // Debouncing
+    if (this.timerId) clearTimeout(this.timerId);
+
+    this.timerId = setTimeout(() => {
+      this.elRef.nativeElement.style.height = window.innerHeight - 16 + "px";
+    }, 10);
+  }
+}
